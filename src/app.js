b0b5e97c884fc11c792bbb4b522a41f39d96af41
siemeris <siemeris@gmail.com>
2022-06-14 08:41:49 +0000

    uploading my work

--- src/app.js
+++ src/app.js
@@ -8,4 +8,33 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let num = document.querySelector(".num");
+  num.innerHTML = numGenerator();
+
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector(".num").innerHTML = numGenerator();
+  });
 };
+
+var simb = ["corazones", "diamantes", "tréboles", "picas"];
+
+function numGenerator() {
+  var numCarta = [
+    "A",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  return numCarta[Math.floor(Math.random() * numCarta.length)];
+}
